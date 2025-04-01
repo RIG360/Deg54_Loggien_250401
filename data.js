55d352d19e078e2fd305765f163e79501a8f8b7d
RIG360 <134787168+RIG360@users.noreply.github.com>
2025-04-01 13:39:10 +0200

    upload

---
+++ data.js
@@ -0,0 +1,201 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-top-5-7-garten",
+      "name": "Top 5-7, Garten",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.017453292519943986,
+        "pitch": -0.040529661178236864,
+        "fov": 1.5418892839542175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.312684233765948,
+          "pitch": -0.6766950144823518,
+          "rotation": 10.995574287564278,
+          "target": "2-top-16-17-loggia"
+        },
+        {
+          "yaw": -1.296873006952353,
+          "pitch": -0.873600004549326,
+          "rotation": 4.71238898038469,
+          "target": "3-top-24-loggia"
+        },
+        {
+          "yaw": 0.3495678328934204,
+          "pitch": 0.29523920051278196,
+          "rotation": 0,
+          "target": "1-top-5-7-loggia"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4780239413926317,
+          "pitch": -0.38131553644725535,
+          "title": "Top 08",
+          "text": "keine Freifläche vorhanden"
+        },
+        {
+          "yaw": 0.4896937331492133,
+          "pitch": -0.7044274290411998,
+          "title": "Top 15",
+          "text": "keine Freifläche vorhanden"
+        },
+        {
+          "yaw": 0.5500639647891745,
+          "pitch": -1.0542019283308441,
+          "title": "Top 20-23",
+          "text": "keine Freifläche vorhanden"
+        }
+      ]
+    },
+    {
+      "id": "1-top-5-7-loggia",
+      "name": "Top 5-7, Loggia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.015216364046226616,
+        "pitch": -0.030005194019746995,
+        "fov": 1.5418892839542175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6677738123542856,
+          "pitch": 0.3602687124110808,
+          "rotation": 3.141592653589793,
+          "target": "0-top-5-7-garten"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8549875330072947,
+          "pitch": 0.8145222372115732,
+          "title": "Fluchtweg aus Heiztechnikzentrale",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-top-16-17-loggia",
+      "name": "Top 16-17, Loggia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.3217352419630668,
+        "pitch": 0.01845292500040685,
+        "fov": 1.5418892839542175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07068578293013061,
+          "pitch": 0.5152015266167549,
+          "rotation": 3.141592653589793,
+          "target": "0-top-5-7-garten"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-top-24-loggia",
+      "name": "Top 24, Loggia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.476817059379817,
+        "pitch": 0.028194546906572526,
+        "fov": 1.5418892839542175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40220961832077684,
+          "pitch": 0.5691091027261752,
+          "rotation": 3.141592653589793,
+          "target": "0-top-5-7-garten"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Deg54, Loggien",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
